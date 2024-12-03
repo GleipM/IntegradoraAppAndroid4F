6166863954984e2d17b4db164e320b8db0e67a92
GleipM <20233tn181@utez.edu.mx>
2024-12-02 23:44:24 -0600

    termine la pantalla bitacora y agregue intent en la pantalla main para poder navegar a la bitacora

--- app/src/main/java/com/example/bitacoravirtual/Bitacora.kt
+++ app/src/main/java/com/example/bitacoravirtual/Bitacora.kt
@@ -1,14 +1,30 @@
 package com.example.bitacoravirtual
 
+import android.R
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.bitacoravirtual.databinding.ActivityBitacoraBinding
+
 
 class Bitacora : AppCompatActivity() {
+    lateinit var binding: ActivityBitacoraBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bitacora)
+        binding= ActivityBitacoraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.edtFecha.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val newFragment = DatePickerFragment.newInstance(DatePickerDialog.OnDateSetListener { _, year, month, day ->
+            // +1 because January is zero
+            val selectedDate = day.toString() + " / " + (month + 1) + " / " + year
+            binding.edtFecha.setText(selectedDate)
+        })
+
+        newFragment.show(supportFragmentManager, "datePicker")
     }
 }
--- app/src/main/java/com/example/bitacoravirtual/MainActivity.kt
+++ app/src/main/java/com/example/bitacoravirtual/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.bitacoravirtual
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bitacoravirtual.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSiguiente.setOnClickListener{
+            //Intent es una clase que nos permite abrir componentes nuevos
+            //intent necesita 2 requisitos
+            //1 Componente origen(Contexto)
+            //2 Componente destino(::class.java)
+            val intent = Intent(this@MainActivity,
+                Bitacora::class.java)
+
+            //enviar un valor de la pantalla a otra
+            //intent.putExtra("valor", binding.edtTexto.text.toString())
+
+            //stack de Activitys
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+
+            //si no se escribe el starActivity no hace nada
+            startActivity(intent)
         }
     }
 }
