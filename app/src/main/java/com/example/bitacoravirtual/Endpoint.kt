6a68854d1b15ada1c76b6348eb43ca8465b7a192
rubengo <20233tn207@utez.edu.mx>
2024-12-13 17:01:18 -0600

    cambios

--- app/src/main/java/com/example/bitacoravirtual/Endpoint.kt
+++ app/src/main/java/com/example/bitacoravirtual/Endpoint.kt
@@ -3,7 +3,7 @@ package com.example.bitacoravirtual
 class Endpoint {
 
     val parte1 = "http://"
-    val ip = "192.168.111.164"
+    val ip = "192.168.110.223"
     val puerto = ":8080"
     val parte2 = "/v1/"
     val endpoint = parte1 + ip + puerto + parte2
